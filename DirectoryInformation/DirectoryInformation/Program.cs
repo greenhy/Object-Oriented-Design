9ba5efce0eea1985e579f04d96a6c7698c892ede
greenhy <63488123+greenhy@users.noreply.github.com>
2020-08-05 19:58:15 +1000

    Add files via upload

---
+++ DirectoryInformation/DirectoryInformation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+namespace DirectoryInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            string[] files;
+            int x;
+            const string END = "end";
+            Write("Enter a directory >> ");
+            directory = ReadLine();
+            while (directory != END)
+            {
+                if (Directory.Exists(directory))
+                {
+                    files = Directory.GetFiles(directory);
+                    if (files.Length == 0)
+                        WriteLine("There are no files in this directory: " +
+                        directory);
+                    else
+                    {
+                        WriteLine(directory + " contains the following files");
+                        for (x = 0; x < files.Length; ++x)
+                            WriteLine(" " + files[x]);
+                    }
+                }
+                else
+                {
+                    WriteLine("Directory " + directory + " does not exist");
+                }
+                Write("\nEnter another directory or type " + END + " to quit >> ");
+                directory = ReadLine();
+            }
+        }
+    }
+}
---
+++ FileComparison/FileComparison/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+
+namespace FileComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string WORD_FILE = @"C:\test.doc";
+            const string NOTEPAD_FILE = @"C:\test.txt";
+            long wordSize;
+            long txtSize;
+            double ratio;
+            FileInfo wordInfo = new FileInfo(WORD_FILE);
+            FileInfo notepadInfo = new FileInfo(NOTEPAD_FILE);
+            wordSize = wordInfo.Length;
+            txtSize = notepadInfo.Length;
+            WriteLine("The size of the Word file: " + wordSize +
+            "\nThe size of the txt file: " + txtSize);
+            ratio = (double) txtSize / wordSize;
+            WriteLine("The txt file is {0} of the size of the Word file",
+            ratio.ToString("P2"));
+        }
+    }
+}
